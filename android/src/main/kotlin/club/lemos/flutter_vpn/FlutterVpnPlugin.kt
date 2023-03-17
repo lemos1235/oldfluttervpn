22d85fca9999456178623437e69234b4c59acf76
lemos <xfe1235@gmail.com>
2023-03-17 15:36:02 +0800

    feat: update package name

--- android/src/main/kotlin/club/lemos/flutter_vpn/FlutterVpnPlugin.kt
+++ android/src/main/kotlin/club/lemos/flutter_vpn/FlutterVpnPlugin.kt
@@ -18,7 +18,7 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
-import club.lemos.android.logic.VpnStateService
+import club.lemos.vpn.logic.VpnStateService
 
 class FlutterVpnPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
